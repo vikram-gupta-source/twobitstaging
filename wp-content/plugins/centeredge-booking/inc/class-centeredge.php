65a148824470282591af68502be1ebfcb8715372
Alex Nguyen <alex@petrolad.com>
2019-09-12 16:03:57 -0700

    Updates

--- wp-content/plugins/centeredge-booking/inc/class-centeredge.php
+++ wp-content/plugins/centeredge-booking/inc/class-centeredge.php
@@ -53,18 +53,18 @@ if ( ! class_exists( 'CenterEdge' ) ) {
                 $count = 0;
                 $arr = $ticket->getElementsByTagName("a");
                 foreach($arr as $key => $item) {
-                  $textRaw = trim(preg_replace("/[\r\n]+/", " ", $item->nodeValue));
-                  $href =  $item->getAttribute("href");
-                  $setTitle = ($count == 0) ? $textRaw : $setTitle; // 2nd Entry has the Title
                   $out = (preg_match('/Out/i', $item->nodeValue)) ? 1 : 0;
-                  $text = trim(str_ireplace('(Out)', '', $textRaw));
+                  $href =  $item->getAttribute("href");
+                  $text = trim(preg_replace("/[\r\n]+/", " ", $item->nodeValue));
+                  $setTitle = ($count == 0) ? $text : $setTitle; // 2nd Entry has the Title
+                  $text = trim(str_ireplace('(Out)', '', $text));
                   if($count > 0) {
-                    $links[$setTitle][$setDate][$key] = [
+                    $links[$setTitle][$setDate][$key]= [
                       'href' => $href,
                       'text' => $text,
                       'out'  => $out
                     ];
-                  }
+                  } 
                   $count++;
                 }
               }
