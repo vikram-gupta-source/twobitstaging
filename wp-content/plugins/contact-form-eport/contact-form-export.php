9957622638fb99fd42080617e039d29f044b992f
Alex Nguyen <alex@petrolad.com>
2019-09-25 13:29:47 -0700

    New Export Plugin

---
+++ wp-content/plugins/contact-form-eport/contact-form-export.php
@@ -0,0 +1,50 @@
+<?php
+/*
+Plugin Name: Contact Form Export
+Plugin URI: http://wordpress.org/plugins/page-list/
+Description: Export Data for COntact Form 7 Cron
+Version: 1.0
+Author: Alex
+Author URI: http://web-profile.com.ua/wordpress/plugins/
+License: GPLv3
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+	//exit; // Exit if accessed directly.
+}
+
+date_default_timezone_set('America/Los_Angeles');
+
+// Activate CenterEdge Book Cron
+function contact_form_export_activation() {
+  if (! wp_next_scheduled ( 'contact_form_export_cron' )) {
+	  //wp_schedule_event(time(), 'daily', 'contact_form_export_cron');
+  }
+}
+//register_activation_hook(__FILE__, 'contact_form_export_activation');
+
+// Remove Cron
+function contact_form_export_deactivation() {
+	//wp_clear_scheduled_hook('contact_form_export_cron');
+}
+//register_deactivation_hook(__FILE__, 'contact_form_export_deactivation');
+
+// Handle Request of CRON
+//add_action( 'contact_form_export_cron', 'contact_form_export' );
+
+//if ( !function_exists( "contact_form_export" ) ) {
+//  function contact_form_export() {
+
+define('WP_USE_THEMES', false);
+require('../../../wp-load.php');
+//print_r(is_admin());
+
+    //if( is_admin() ){
+        require_once 'inc/export-csv.php';
+        $csv = new Expoert_CSV();
+        //if( isset($_REQUEST['csv']) && ( $_REQUEST['csv'] == true ) && isset( $_REQUEST['nonce'] ) ) {
+        $csv->send_csv_email();
+        //}
+  //  }
+//  }
+//}
---
+++ wp-content/plugins/contact-form-eport/inc/export-csv.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * CFDB7 csv
+ */
+
+//if (!defined( 'ABSPATH')) exit;
+
+class Expoert_CSV{
+
+    public $fid = 431;
+    private $cfdb;
+    private $table_name;
+
+    public function __construct() {
+      global $wpdb;
+      $this->cfdb        = apply_filters( 'cfdb7_database', $wpdb );
+      $this->table_name  = $this->cfdb->prefix.'db7_forms';
+    }
+    public function get_csv_heading() {
+      $heading_row = $this->cfdb->get_results("SELECT form_id, form_value, form_date FROM $this->table_name WHERE form_post_id = '$this->fid' ORDER BY form_id DESC LIMIT 1", OBJECT);
+      $heading_row = reset( $heading_row );
+      $heading_row = unserialize( $heading_row->form_value );
+      $heading_key = array_keys( $heading_row );
+      return $heading_key;
+    }
+    public function get_email_listing() {
+      $data = [];
+      $rows = $this->cfdb->get_results("SELECT form_value FROM $this->table_name WHERE form_post_id = '$this->fid' AND DATE_FORMAT(form_date, '%Y-%m-%d') = (CURDATE() - INTERVAL 1 DAY) ORDER BY form_id DESC", OBJECT);
+      foreach($rows as $row) {
+        $rowRaw = unserialize( $row->form_value );
+        $data[$rowRaw['inquiry']][] = $rowRaw;
+      }
+      return $data;
+    }
+    public function create_csv_string($data, $heading_key) {
+      if (!$fp = fopen('php://temp', 'w+')) return FALSE;
+      fputcsv($fp, $heading_key);
+      // Loop data and write to file pointer
+      foreach ($data as $line) fputcsv($fp, $line);
+      rewind($fp);
+      return stream_get_contents($fp);
+    }
+
+    public function send_csv_email() {
+      $heading_key = $this->get_csv_heading();
+      $emailRows = $this->get_email_listing();
+      $subject = 'Daily Inquiries';
+      $message = '';
+      foreach($emailRows as $to => $data) {
+        $headers = 'From: Two Bit Circus <'.$to.'>' . "\r\n";
+        $attachments = chunk_split(base64_encode($this->create_csv_string($data, $heading_key)));
+        wp_mail( 'alex@petrolad.com', $subject, $message, $headers, $attachments );
+      }
+      die();
+    }
+}
