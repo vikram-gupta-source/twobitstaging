0f24170ea934a57f1323663cdba0e6c94fbe6b5a
Alex Nguyen <alex@petrolad.com>
2019-10-01 11:52:02 -0700

    Updates Plugin Form COntat

--- wp-content/plugins/contact-form-export/inc/export-csv.php
+++ wp-content/plugins/contact-form-export/inc/export-csv.php
@@ -25,7 +25,7 @@ class Expoert_CSV{
     }
     public function get_email_listing() {
       $data = [];
-      $rows = $this->cfdb->get_results("SELECT form_value FROM $this->table_name WHERE form_post_id = '$this->fid' AND DATE_FORMAT(form_date, '%Y-%m-%d') = CURDATE() ORDER BY form_id DESC", OBJECT);
+      $rows = $this->cfdb->get_results("SELECT form_value FROM $this->table_name WHERE form_post_id = '$this->fid' AND DATE_FORMAT(form_date, '%Y-%m-%d') = (CURDATE() - INTERVAL 7 DAY) ORDER BY form_id DESC", OBJECT);
       foreach($rows as $row) {
         $rowRaw = unserialize( $row->form_value );
         $data[$rowRaw['inquiry']][] = $this->clean_value($rowRaw);
@@ -49,34 +49,37 @@ class Expoert_CSV{
     }
 
     public function send_csv_email() {
-      $heading_key = $this->get_csv_heading();
-      $emailRows = $this->get_email_listing();
-      $setDate = date("m/d/Y");
-      $subject = 'Daily Inquiries for ' . $setDate;
-      $multipartSep = '-----'.md5(time()).'-----';
-      foreach($emailRows as $to => $data) {
-        $attachment = chunk_split(base64_encode($this->create_csv_string($data, $heading_key)));
-        $headers = array(
-           'From: Two Bit Circus <'.$to.'>' ,
-           "Content-Type: multipart/mixed; boundary=\"$multipartSep\""
-         );
-        // Make the body of the message
-        $body = "--$multipartSep\r\n"
-              . "Content-Type: text/plain; charset=ISO-8859-1; format=flowed\r\n"
-              . "Content-Transfer-Encoding: 7bit\r\n"
-              . "\r\n"
-              . "$subject\r\n"
-              . "--$multipartSep\r\n"
-              . "Content-Type: text/csv\r\n"
-              . "Content-Transfer-Encoding: base64\r\n"
-              . "Content-Disposition: attachment; filename=\"Daily_Inquiries_".date("m_d_Y").".csv\"\r\n"
-              . "\r\n"
-              . "$attachment\r\n"
-              . "--$multipartSep--";
+      $sendto = get_field('daily_email_address', 'options');
+      if(!empty($sendto)) {
+        $heading_key = $this->get_csv_heading();
+        $emailRows = $this->get_email_listing();
+        $setDate = date("m/d/Y");
+        $subject = 'Daily Inquiries for ' . $setDate;
+        $multipartSep = '-----'.md5(time()).'-----';
+        foreach($emailRows as $to => $data) {
+          $attachment = chunk_split(base64_encode($this->create_csv_string($data, $heading_key)));
+          $headers = array(
+             'From: Two Bit Circus <'.$to.'>' ,
+             "Content-Type: multipart/mixed; boundary=\"$multipartSep\""
+           );
+          // Make the body of the message
+          $body = "--$multipartSep\r\n"
+                . "Content-Type: text/plain; charset=ISO-8859-1; format=flowed\r\n"
+                . "Content-Transfer-Encoding: 7bit\r\n"
+                . "\r\n"
+                . "$subject\r\n"
+                . "--$multipartSep\r\n"
+                . "Content-Type: text/csv\r\n"
+                . "Content-Transfer-Encoding: base64\r\n"
+                . "Content-Disposition: attachment; filename=\"Daily_Inquiries_".date("m_d_Y").".csv\"\r\n"
+                . "\r\n"
+                . "$attachment\r\n"
+                . "--$multipartSep--";
 
-         // Send the email, return the result
-        @mail($to, $subject, $body, implode("\r\n", $headers));
-        sleep(1);
+           // Send the email, return the result
+          @mail($sendto, $subject, $body, implode("\r\n", $headers));
+          sleep(1);
+        }
       }
       die();
     }
