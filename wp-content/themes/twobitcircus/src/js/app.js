8a0cd153e22cb15b68a6c76f9490c9515879cdd5
petroldev <alex@petrolad.com>
2019-10-11 15:28:29 -0700

    Update Modal

--- wp-content/themes/twobitcircus/src/js/app.js
+++ wp-content/themes/twobitcircus/src/js/app.js
@@ -293,6 +293,17 @@ $(function () {
     }
   );
 
+  // Custom Modal
+  if($(".custom-modal").length) {
+    $(".custom-modal").on('click', function (e) {
+      e.preventDefault();
+      var $_this = $(this);
+      let html = '<div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="' + $_this.attr('href') + '" allowfullscreen></iframe></div>';
+      $('#custom-modal').find('.modal-body').html(html);
+      $('#custom-modal').modal('show');
+    });
+  }
+
   // Handle CLick for Nav Map
   $(".nav-link-direction").on("click", function (evt) {
     evt.preventDefault();
@@ -1049,18 +1060,5 @@ $(function () {
       }
     });
     return false;
-  }
-  if($(".custom-modal").length) {
-    alert(1)
-    $(".custom-modal").on('click', function (e) {
-      alert(5)
-      e.preventDefault();
-      var $_this = $(this);
-      $('#custom-modal').modal('show');
-      $('#custom-modal').on('show.bs.modal', function (event) {
-        let html = '<div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="' + $_this.attr('href') + '" allowfullscreen></iframe></div>';
-        $(this).find('.modal-body').html(html);
-      });
-    });
   }
 });
--- wp-content/themes/twobitcircus/template-parts/partial/partial-modal.php
+++ wp-content/themes/twobitcircus/template-parts/partial/partial-modal.php
@@ -7,7 +7,7 @@
 ?>
 <!-- Modal -->
 <div class="modal fade" id="custom-modal" tabindex="-1" role="dialog" aria-labelledby="custom-modal" aria-hidden="true">
-  <div class="modal-dialog" role="document">
+  <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <h6 class="modal-title"></h6>
