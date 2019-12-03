dec5b5ae0fa9a2749ad5bd1a5c04238c9b12f78a
Alex Nguyen <nalex826@gmail.com>
2019-12-03 13:42:30 -0800

    Updates

--- wp-content/themes/twobitcircus/template-parts/content/content-attractions.php
+++ wp-content/themes/twobitcircus/template-parts/content/content-attractions.php
@@ -78,8 +78,9 @@
                           <?php if(!empty($_video)) :?>
                           <?php $videoPart = explode(',', $_video);?>
                           <?php foreach($videoPart as $_vid) :?>
+                          <?php $videoThumb = videoLink($_vid, true);?>
                           <div class="item d-block">
-                            <div class="embed-lazy embed-responsive embed-responsive-16by9" data-video="https://player.vimeo.com/video/<?php echo trim($_vid);?>"></div>
+                            <div class="thumb pre-load-img" data-img="<?php echo $videoThumb; ?>"><img class="img-fluid" alt="<?php echo $show->post_title;?>"/></div>
                           </div>
                           <?php endforeach ?>
                           <?php endif ?>
