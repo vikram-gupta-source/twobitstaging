c3ceedd60b90422e860508fde41c5a57dbd9e329
Alex Nguyen <nalex826@gmail.com>
2020-02-27 15:24:19 -0800

    Update

--- wp-content/themes/twobitcircus/inc/class-calendar.php
+++ wp-content/themes/twobitcircus/inc/class-calendar.php
@@ -44,8 +44,7 @@ if ( ! class_exists( 'Calendar' ) ) {
       if(!empty($data)) {
         print_r($this->allowShows);
         foreach($data as $entry) {
-          echo $entry->name . '--';
-          if(in_array($entry->name, $this->allowShows)) {
+          if(in_array(trim($entry->name), $this->allowShows)) {
             echo $entry->name . '++';
             $_day = explode('-',$entry->posted)[1];
             $this->calender[$_day][ date('H:i A', strtotime($entry->ticket))][] = $entry;
