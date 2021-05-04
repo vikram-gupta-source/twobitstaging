4f1c1446766e67927981bd819b31326c50869656
Alex Nguyen <alex@petrolad.com>
2021-05-03 17:35:50 -0700

    Update Instagram

--- wp-content/plugins/instagram-feeds/inc/instagrams.class.php
+++ wp-content/plugins/instagram-feeds/inc/instagrams.class.php
@@ -4,28 +4,28 @@
  *
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
+if (! defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
 }
 
 /* Check if Class Exists. */
-if ( ! class_exists( 'Instagrams' ) ) {
+if (! class_exists('Instagrams')) {
+    class Instagrams
+    {
+        public $hash = '';
+        public $json = '';
+        public $limit = 10;
 
-  class Instagrams {
-
-    public $hash = '';
-    public $json = '';
-    public $limit = 10;
-
-    public function load_instagram_feed() {
-      $data = [];
-      $results_array = json_decode($this->json, TRUE);
-      if(!empty($results_array["graphql"])) {
-        for ($i=$this->limit; $i >= 0; $i--) {
-          if(array_key_exists($i,$results_array["graphql"]["user"]["edge_felix_video_timeline"]["edges"])){
-            $latest_array = $results_array["graphql"]["user"]["edge_felix_video_timeline"]["edges"][$i]["node"];
-            $url = $this->saveImg($latest_array['thumbnail_src'], $latest_array['id']);
-            $newPosting = [
+        public function load_instagram_feed()
+        {
+            $data = [];
+            $results_array = json_decode($this->json, true);
+            if (!empty($results_array["graphql"])) {
+                for ($i=$this->limit; $i >= 0; $i--) {
+                    if (array_key_exists($i, $results_array["graphql"]["user"]["edge_owner_to_timeline_media"]["edges"])) {
+                        $latest_array = $results_array["graphql"]["user"]["edge_owner_to_timeline_media"]["edges"][$i]["node"];
+                        $url = $this->saveImg($latest_array['thumbnail_src'], $latest_array['id']);
+                        $newPosting = [
               "pubdate"=>$latest_array['taken_at_timestamp'],
               "instagram_id" => $latest_array['id'],
               "link"=>"https://www.instagram.com/p/".$latest_array['shortcode'],
@@ -35,49 +35,53 @@ if ( ! class_exists( 'Instagrams' ) ) {
               'count2' => (isset($latest_array['video_view_count'])) ? $latest_array['video_view_count'] : $latest_array['edge_liked_by']['count'],
               'status' => 0
             ];
-            array_push($data, $newPosting);
-          }
-        }
-        $this->insta_write($data);
-        add_action('admin_notices', function($messages){
-          ?>
+                        array_push($data, $newPosting);
+                    }
+                }
+                $this->insta_write($data);
+                add_action('admin_notices', function ($messages) {
+                    ?>
           <div class="notice notice-success"><p>Instagram Feed has been process.</p></div>
           <?php
-        });
-      } else {
-        add_action('admin_notices', function($messages){
-          ?>
+                });
+            } else {
+                add_action('admin_notices', function ($messages) {
+                    ?>
           <div class="notice notice-error"><p>Instagram Feed failed to processed.</p></div>
           <?php
-        });
-      }
+                });
+            }
+        }
+        public function instagram_custom_warning_filter()
+        {
+        }
+        private function saveImg($img, $id)
+        {
+            $parts = explode('?', $img);
+            $pathinfo = pathinfo($parts[0]);
+            $url =  $id.'.'.$pathinfo['extension'];
+            $folder = ABSPATH . '/wp-content/uploads/instagram';
+            if (!is_dir($folder)) {
+                mkdir($folder, 0755, true);
+            }
+            $filepath = $folder . '/'.$url;
+            $image = imagecreatefromjpeg($img);
+            imagejpeg($image, $filepath, 75);
+            imagedestroy($image);
+            return '/wp-content/uploads/instagram/' . $url;
+        }
+        private function insta_write($data=null)
+        {
+            global $wpdb;
+            if (empty($data)) {
+                return null;
+            }
+            $table_name = $wpdb->prefix . 'instagram_feeds';
+            foreach ($data  as $entry) {
+                $wpdb->replace($table_name, $entry, array('%d', '%d', '%s', '%s', '%s', '%d', '%d', '%d'));
+            }
+            return 'passed';
+        }
     }
-    public function instagram_custom_warning_filter() {
-
-    }
-    private function saveImg($img, $id) {
-      $parts = explode('?', $img);
-      $pathinfo = pathinfo($parts[0]);
-      $url =  $id.'.'.$pathinfo['extension'];
-      $folder = ABSPATH . '/wp-content/uploads/instagram';
-      if (!is_dir($folder)) {
-        mkdir($folder, 0755, true);
-      }
-      $filepath = $folder . '/'.$url;
-      $image = imagecreatefromjpeg($img);
-      imagejpeg($image, $filepath, 75);
-      imagedestroy($image);
-      return '/wp-content/uploads/instagram/' . $url;
-    }
-    private function insta_write($data=null) {
-      global $wpdb;
-      if(empty($data)) return null;
-      $table_name = $wpdb->prefix . 'instagram_feeds';
-      foreach($data  as $entry) {
-        $wpdb->replace($table_name, $entry, array('%d', '%d', '%s', '%s', '%s', '%d', '%d', '%d'));
-      }
-      return 'passed';
-    }
-  }
 }
 ?>
