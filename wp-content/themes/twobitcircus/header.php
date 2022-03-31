44b6e064d4edea58206727a3d4b2d8fe48331ab7
Alex Nguyen <alex@petrolad.com>
2022-03-31 16:22:41 -0700

    Update

--- wp-content/themes/twobitcircus/header.php
+++ wp-content/themes/twobitcircus/header.php
@@ -57,25 +57,6 @@
 
   <!-- CRAZYEGG Tracking -->
   <script type="text/javascript" src="//script.crazyegg.com/pages/scripts/0078/9623.js" async="async"></script>
-  <!-- Facebook Pixel Code -->
-  <script>
-    !function(f,b,e,v,n,t,s)
-    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-    n.queue=[];t=b.createElement(e);t.async=!0;
-    t.src=v;s=b.getElementsByTagName(e)[0];
-    s.parentNode.insertBefore(t,s)}(window,document,'script',
-    'https://connect.facebook.net/en_US/fbevents.js');
-     fbq('init', '1869285869829604');
-     fbq('track', 'PageView');
-  </script>
-  <noscript>
-   <img height="1" width="1"
-  src="https://www.facebook.com/tr?id=1869285869829604&ev=PageView
-  &noscript=1"/>
-  </noscript>
-  <!-- End Facebook Pixel Code -->
 </head>
 
 <body <?php body_class(); ?>>
