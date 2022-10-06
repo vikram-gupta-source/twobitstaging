a345b985eedc16327ae98d01ce95065caa36f6cb
Alex Nguyen <alex@petrolad.com>
2022-10-05 17:54:02 -0700

    1

--- wp-content/themes/twobitcircus/inc/setup.php
+++ wp-content/themes/twobitcircus/inc/setup.php
@@ -269,7 +269,7 @@ function posts_link_attributes()
 // Handle Region & filters
 // Collect Region
 function twobitcircus_load_location() {
-  global $location, $region, $geo;
+  global $geo;
   print_r($_COOKIE['geo_location']);
   if(!isset($_COOKIE['geo_location'])) {
     $region = $geo->get_location_by_ip();
