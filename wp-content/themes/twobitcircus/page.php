8573fdb9101b93f84426c97e81c4dadde66e4338
Alex Nguyen <alex@petrolad.com>
2022-11-15 14:13:47 -0800

    Test

--- wp-content/themes/twobitcircus/page.php
+++ wp-content/themes/twobitcircus/page.php
@@ -7,55 +7,57 @@
 get_header();
 ?>
 
-	<section id="page-template">
+    <section id="page-template">
 
-			<?php
+            <?php
 
-			/* Start the Loop */
-			while ( have_posts() ) :
-        // Get Hero Template
-        get_template_part( 'template-parts/partial/partial', 'hero' );
+            /* Start the Loop */
+            while ( have_posts() ) :
+                // Get Hero Template
+                get_template_part('template-parts/partial/partial', 'hero');
 
-				the_post();
-        if(is_front_page()) {
-          get_template_part( 'template-parts/content/content', 'home' );
-        } elseif(is_page('about')) {
-          get_template_part( 'template-parts/content/content', 'about' );
-        } elseif(is_page('food-drink')) {
-          get_template_part( 'template-parts/content/content', 'food' );
-        } elseif((is_page('promotions')) || (is_page('events'))) {
-          get_template_part( 'template-parts/content/content', 'promo' );
-        } elseif(is_page('attractions')) {
-          get_template_part( 'template-parts/content/content', 'attractions' );
-        } elseif((is_page('parties-events')) || (is_page('birthday'))) {
-          get_template_part( 'template-parts/content/content', 'parties' );
-        } elseif(is_page('media')) {
-          get_template_part( 'template-parts/content/content', 'media' );
-        } elseif(is_page('contact')) {
-          get_template_part( 'template-parts/content/content', 'contact' );
-        } elseif(is_page('jobs')) {
-          get_template_part( 'template-parts/content/content', 'jobs' );
-        } elseif(is_page('locations')) {
-          get_template_part( 'template-parts/content/content', 'locations' );
-        } elseif(is_page('latest-news')) {
-          get_template_part( 'template-parts/content/content', 'news' );
-        } elseif(is_page('our-manifesto')) {
-          get_template_part( 'template-parts/content/content', 'manifesto' );
-        } elseif(is_page('blog')) {
-          get_template_part( 'template-parts/content/content', 'blog' );
-				} elseif(is_page('faq')) {
-          get_template_part( 'template-parts/content/content', 'faqs' );
-        } else {
-          get_template_part( 'template-parts/content/content' );
-        }
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) {
-					comments_template();
-				}
+                the_post();
+                if(is_front_page()) {
+                    get_template_part('template-parts/content/content', 'home');
+                } elseif(is_page('about')) {
+                    get_template_part('template-parts/content/content', 'about');
+                } elseif(is_page('food-drink')) {
+                    get_template_part('template-parts/content/content', 'food');
+                } elseif((is_page('promotions')) || (is_page('events'))) {
+                    get_template_part('template-parts/content/content', 'promo');
+                } elseif(is_page('attractions')) {
+                    get_template_part('template-parts/content/content', 'attractions');
+                } elseif((is_page('parties-events')) || (is_page('birthday'))) {
+                    get_template_part('template-parts/content/content', 'parties');
+                } elseif(is_page('media')) {
+                    get_template_part('template-parts/content/content', 'media');
+                } elseif(is_page('contact')) {
+                    get_template_part('template-parts/content/content', 'contact');
+                } elseif(is_page('jobs')) {
+                    get_template_part('template-parts/content/content', 'jobs');
+                } elseif(is_page('locations')) {
+                    get_template_part('template-parts/content/content', 'locations');
+                } elseif(is_page('dallas')) {
+                    get_template_part('template-parts/content/content', 'dallas');
+                } elseif(is_page('latest-news')) {
+                    get_template_part('template-parts/content/content', 'news');
+                } elseif(is_page('our-manifesto')) {
+                    get_template_part('template-parts/content/content', 'manifesto');
+                } elseif(is_page('blog')) {
+                    get_template_part('template-parts/content/content', 'blog');
+                } elseif(is_page('faq')) {
+                    get_template_part('template-parts/content/content', 'faqs');
+                } else {
+                    get_template_part('template-parts/content/content');
+                }
+                // If comments are open or we have at least one comment, load up the comment template.
+                if (comments_open() || get_comments_number() ) {
+                    comments_template();
+                }
 
-      endwhile; ?>
+            endwhile; ?>
 
-	</section><!-- #primary -->
+    </section><!-- #primary -->
 
 <?php
 get_footer();
--- wp-content/themes/twobitcircus/template-parts/content/content-promo.php
+++ wp-content/themes/twobitcircus/template-parts/content/content-promo.php
@@ -9,7 +9,7 @@
 ?>
 <article id="promotions" <?php post_class(); ?>>
 
-  <?php get_template_part( 'template-parts/partial/partial', 'header' ); ?>
+  <?php get_template_part('template-parts/partial/partial', 'header'); ?>
 
   <!-- Go to www.addthis.com/dashboard to customize your tools -->
   <script defer async type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5d40d77186a2f4a8"></script>
@@ -17,16 +17,16 @@
     <?php if(!empty($promotions)) :?>
 
     <div class="container-fluid">
-      <?php if(!empty($promotionContent[0])) :?>
+        <?php if(!empty($promotionContent[0])) :?>
       <div class="content-block container mt-2 mb-6 px-md-5">
         <h2 class="default white"><?php echo $promotionContent[0]['title'];?></h2>
         <div class="white"><?php echo $promotionContent[0]['content'];?></div>
       </div>
-      <?php endif ?>
+        <?php endif ?>
       <div class="grid-flex">
         <?php foreach($promotions as $promo) : ?>
         <div class="grid-item card">
-          <?php if(!empty($promo['video_embed'])) :?>
+            <?php if(!empty($promo['video_embed'])) :?>
           <div class="embed-responsive embed-responsive-16by9"><iframe src="<?php echo $promo['video_embed'];?>" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen="" data-ready="true"></iframe>
           </div>
           <?php else: ?>
@@ -50,10 +50,10 @@
               <a class="addthis_button_twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a>
             </div>
             <div class="link-wrapper my-2">
-              <?php if(!empty($promo['link'])):?>
-              <?php $isInternalLink = checkInternalLink($promo['link']);?>
+                <?php if(!empty($promo['link'])) :?>
+                    <?php $isInternalLink = checkInternalLink($promo['link']);?>
               <div class="cta-btn mx-auto"><a class="btn btn-twobit" href="<?php echo $promo['link'];?>" <?php echo $isInternalLink;?>><span><?php echo (!empty($promo['link_title'])) ? $promo['link_title'] :'Book Now'; ?></span></a><div class="btn-behind">&nbsp;</div></div>
-              <?php endif ?>
+                <?php endif ?>
             </div>
             <?php endif ?>
           </div>
