de634cc4a47352924722b86dcd26887a5f8d6743
Alex Nguyen <alex@petrolad.com>
2022-12-10 10:35:56 -0800

    1

--- wp-content/themes/twobitcircus/template-parts/content/content-parties.php
+++ wp-content/themes/twobitcircus/template-parts/content/content-parties.php
@@ -4,80 +4,83 @@
  *
  * @package twobitcircus
  */
- global $location;
- remove_filter('the_content', 'wpautop');
- $contactForm = filter_locations(get_field('contact_form', 'option'));
+global $location;
+remove_filter('the_content', 'wpautop');
+remove_filter('contact_form', 'wpautop');
+$contactForm = filter_locations(get_field('contact_form', 'option'));
 ?>
-<article id="parties" <?php post_class(); ?>>
+<article id="parties" <?php post_class();?>>
 
-  <?php get_template_part('template-parts/partial/partial', 'header'); ?>
+    <?php get_template_part('template-parts/partial/partial', 'header');?>
 
-  <section id="parties-block" class="entry-wrapper-padding inview animated delay-3">
-    <?php if(!empty(get_field('event'))) :?>
+    <section id="parties-block" class="entry-wrapper-padding inview animated delay-3">
+        <?php if (!empty(get_field('event'))): ?>
         <?php $events = filter_locations(get_field('event'));?>
-    <div class="container-fluid">
-      <div class="grid-flex">
-        <?php foreach($events as $shows) : ?>
-        <div class="grid-item card" data-loc="<?php echo $location['state_abrv'];?>" data-event="<?php echo $shows['event_type'];?>" id="<?php echo sanitize_title($shows['title']);?>">
-            <?php if(!empty($shows['video_embed'])) :?>
-          <div class="embed-responsive embed-responsive-16by9"><iframe src="<?php echo $shows['video_embed'];?>" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen="" data-ready="true"></iframe>
-          </div>
-          <?php else: ?>
-          <a href="#" class="event-link card-image"><img class="img-fluid w-100" src="<?php echo $shows['event_image']; ?>"/></a>
-          <?php endif ?>
-            <?php if(!empty($shows['title'])) :?>
-          <div class="card-title mb-0 d-flex justify-content-between align-items-center event-link">
-            <h5 class="lubalin"><?php echo $shows['title'];?>
-                <?php if(!empty($shows['event_subtitle'])) :?>
-            <br><span class="roboto"><?php echo $shows['event_subtitle'];?></span>
-                <?php endif ?>
-            </h5>
-            <i class="fa fa-lg fa-angle-down" aria-hidden="true"></i>
-          </div>
-            <?php endif ?>
-          <div class="card-body text-left collapse fade">
-            <?php if(!empty($shows['event_summary'])) :?>
-            <p><?php echo $shows['event_summary'];?></p>
-            <?php endif ?>
-            <div class="link-wrapper my-2">
-              <div class="cta-btn mx-auto"><a class="btn btn-twobit" data-toggle="modal" data-target="#event-form" href="#"><span><?php echo $shows['event_cta_title'];?></span></a><div class="btn-behind">&nbsp;</div></div>
+        <div class="container-fluid">
+            <div class="grid-flex">
+                <?php foreach ($events as $shows): ?>
+                <div class="grid-item card" data-loc="<?php echo $location['state_abrv']; ?>" data-event="<?php echo $shows['event_type']; ?>" id="<?php echo sanitize_title($shows['title']); ?>">
+                    <?php if (!empty($shows['video_embed'])): ?>
+                    <div class="embed-responsive embed-responsive-16by9"><iframe src="<?php echo $shows['video_embed']; ?>" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen="" data-ready="true"></iframe>
+                    </div>
+                    <?php else: ?>
+                    <a href="#" class="event-link card-image"><img class="img-fluid w-100" src="<?php echo $shows['event_image']; ?>" /></a>
+                    <?php endif?>
+                    <?php if (!empty($shows['title'])): ?>
+                    <div class="card-title mb-0 d-flex justify-content-between align-items-center event-link">
+                        <h5 class="lubalin"><?php echo $shows['title']; ?>
+                            <?php if (!empty($shows['event_subtitle'])): ?>
+                            <br><span class="roboto"><?php echo $shows['event_subtitle']; ?></span>
+                            <?php endif?>
+                        </h5>
+                        <i class="fa fa-lg fa-angle-down" aria-hidden="true"></i>
+                    </div>
+                    <?php endif?>
+                    <div class="card-body text-left collapse fade">
+                        <?php if (!empty($shows['event_summary'])): ?>
+                        <p><?php echo $shows['event_summary']; ?></p>
+                        <?php endif?>
+                        <div class="link-wrapper my-2">
+                            <div class="cta-btn mx-auto"><a class="btn btn-twobit" data-toggle="modal" data-target="#event-form" href="#"><span><?php echo $shows['event_cta_title']; ?></span></a>
+                                <div class="btn-behind">&nbsp;</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <?php endforeach?>
             </div>
-          </div>
         </div>
-        <?php endforeach ?>
-      </div>
-    </div>
-    <?php endif ?>
-  </section>
+        <?php endif?>
+    </section>
 
-  <div class="modal fade" id="event-form" tabindex="-1" role="dialog" aria-labelledby="event-form" aria-hidden="true">
-    <div class="modal-dialog modal-lg" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title"></h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
+    <div class="modal fade" id="event-form" tabindex="-1" role="dialog" aria-labelledby="event-form" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title"></h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <?php foreach ($contactForm as $form): ?>
+                    <?php echo $form['form']; ?>
+                    <?php endforeach?>
+                </div>
+            </div>
         </div>
-        <div class="modal-body">
-          <?php foreach($contactForm as $form) : ?>
-                <?php echo $form['form'];?>
-          <?php endforeach ?>
-        </div>
-      </div>
     </div>
-  </div>
 
-  <?php if(!empty(get_field('footer_block'))) :?>
-        <?php $footer = filter_locations(get_field('footer_block'));?>
-        <?php foreach($footer as $banner) : ?>
-  <section id="footer-event-block" class="entry-wrapper-padding inview animated delay bkg-img" style="background-image: url('<?php echo $banner['image'];?>');">
-    <div class="container text-center">
-      <h2 class="headline text-uppercase"><?php echo $banner['title'];?></h2>
-            <?php echo apply_filters('the_content', $banner['description']);?>
-    </div>
-  </section>
-        <?php endforeach ?>
-  <?php endif ?>
+    <?php if (!empty(get_field('footer_block'))): ?>
+    <?php $footer = filter_locations(get_field('footer_block'));?>
+    <?php foreach ($footer as $banner): ?>
+    <section id="footer-event-block" class="entry-wrapper-padding inview animated delay bkg-img" style="background-image: url('<?php echo $banner['image']; ?>');">
+        <div class="container text-center">
+            <h2 class="headline text-uppercase"><?php echo $banner['title']; ?></h2>
+            <?php echo apply_filters('the_content', $banner['description']); ?>
+        </div>
+    </section>
+    <?php endforeach?>
+    <?php endif?>
 
 </article>
